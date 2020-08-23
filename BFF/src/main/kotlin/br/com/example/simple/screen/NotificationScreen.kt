de6fedad0189a9301f8290bc30e607ecfca17a5e
luisgustavoB1 <luisgustavooliveirasilva2016@gmail.com>
2020-08-23 13:08:14 -0300

    create screen notification

--- BFF/src/main/kotlin/br/com/example/simple/screen/NotificationScreen.kt
+++ BFF/src/main/kotlin/br/com/example/simple/screen/NotificationScreen.kt
@@ -1,15 +1,17 @@
 package br.com.example.simple.screen
 
+import br.com.example.simple.compose.NotificationTemplateCompose
+import br.com.example.simple.constants.LIGHT_BACKGROUND
 import br.com.example.simple.constants.STYLE_NAVIGATION_BAR
-import br.com.zup.beagle.widget.layout.Container
-import br.com.zup.beagle.widget.layout.NavigationBar
-import br.com.zup.beagle.widget.layout.Screen
-import br.com.zup.beagle.widget.layout.ScreenBuilder
-import br.com.zup.beagle.widget.ui.Text
+import br.com.zup.beagle.core.Style
+import br.com.zup.beagle.ext.applyFlex
+import br.com.zup.beagle.widget.core.Flex
+import br.com.zup.beagle.widget.layout.*
 
 object NotificationScreen: ScreenBuilder {
     override fun build(): Screen {
         return Screen(
+                style = Style(backgroundColor = LIGHT_BACKGROUND),
                 navigationBar = NavigationBar(
                         "Notificações",
                         showBackButton = true,
@@ -17,9 +19,27 @@ object NotificationScreen: ScreenBuilder {
                 ),
                 child = Container(
                         children = listOf(
-                                Text("Implement me")
+                                notification
                         )
-                )
+                ).applyFlex(flex = Flex(grow = 1.0))
         )
     }
+
+    private val notification = ScrollView(
+            scrollBarEnabled = false,
+            children = listOf(
+                    NotificationTemplateCompose(
+                            title = "Bem Vindo",
+                            mensage = "Seja bem-vindo, colega! Desejamos que você se sinta bem entre nós, que faça parte das nossas vivências, que encontre oportunidades e condições para aprender e crescer. ... Esperamos sinceramente que a sua experiência conosco seja excelente e duradoura."
+                    ),
+                    NotificationTemplateCompose(
+                            title = "Dia do Trabalhador",
+                            mensage = "Todo trabalho honesto sustenta, honra e dignifica o ser humano. Feliz Dia do Trabalho! Parabéns trabalhador! Pela força, coragem e dedicação em fazer um trabalho digno e de extrema importância."
+                    ),
+                    NotificationTemplateCompose(
+                            title = "Dia do Comerciante",
+                            mensage = "Quando vendemos nossa mercadoria, fazemos mais do que trocar ideias ou diminuir barreiras… Nós criamos desenvolvimento e buscamos novas amizades! O comércio é um grande impulsionador da sociedade, sem dúvida! Feliz dia do comerciante!"
+                    )
+            )
+    )
 }
